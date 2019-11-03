7872b041f68ea2086d000c9f7efc92124bbcb7cc
Janez Cimerman <j.cimko@gmail.com>
2019-11-03 13:11:08 +0100

    Importing and showing of 2 stereo images

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Homework workspace for 3D sensing and sensor fusion course
+
+## Running 
+
+    rosrun homework1 homework1_node PATH_TO_LEFT_IMAGE PATH_TO_RIGHT_IMAGE
--- src/homework1/src/homework1_node.cpp
+++ src/homework1/src/homework1_node.cpp
@@ -8,25 +8,86 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char ** argv)
-{
-  if (argc != 2)
-  {
+int main(int argc, char ** argv){
+  if (argc != 3){
     cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
     return -1;
   }
 
-  Mat RGBimage;
+  Mat LIMG;
+  Mat RIMG;
   Mat GSimage;
-  RGBimage = imread(argv[1]);  // Read the file
+  LIMG = imread(argv[1]);
+  RIMG = imread(argv[2]);
 
-  if (!RGBimage.data)
-  {
-    cout << "Could not open or find the image" << endl;
+  if (!LIMG.data){
+    cout << "Could not open or find the left image" << endl;
     return -1;
   }
-  else
-  {
-    cout << "Imported the image :)" << endl;
+  else{
+    cout << "Imported the left image :)" << endl;
   }
+  
+  if (!RIMG.data){
+    cout << "Could not open or find the right image" << endl;
+    return -1;
+  }
+  else{
+    cout << "Imported the right image :)" << endl;
+  }
+
+
+
+  Mat origCombined;
+  Mat scaledLIMG;
+  Mat scaledRIMG;
+  
+  resize(LIMG, scaledLIMG, cv::Size(), 0.25, 0.25);
+  resize(RIMG, scaledRIMG, cv::Size(), 0.25, 0.25);
+  
+  hconcat(scaledLIMG,scaledRIMG,origCombined);
+
+  int key;
+  while (1){
+    key = waitKey(30);
+
+    if (key == 27){
+      // if "esc" is pressed end the program
+      std::cout << "Closing the program because esc pressed \n";
+      break;
+    }
+    imshow("original", origCombined);
+
+    
+  }
+
+  return 0;
 }
+
+/*
+import numpy
+ 
+fx = 942.8        # lense focal length
+baseline = 54.8   # distance in mm between the two cameras
+disparities = 64  # num of disparities to consider
+block = 15        # block size to match
+units = 0.001     # depth units
+ 
+for i in xrange(block, left.shape[0] - block - 1):
+    for j in xrange(block + disparities, left.shape[1] - block - 1):
+        ssd = numpy.empty([disparities, 1])
+ 
+        # calc SSD at all possible disparities
+        l = left[(i - block):(i + block), (j - block):(j + block)]
+        for d in xrange(0, disparities):
+            r = right[(i - block):(i + block), (j - d - block):(j - d + block)]
+            ssd[d] = numpy.sum((l[:,:]-r[:,:])**2)
+ 
+        # select the best match
+        disparity[i, j] = numpy.argmin(ssd)
+ 
+# Convert disparity to depth
+depth = np.zeros(shape=left.shape).astype(float)
+depth[disparity &gt; 0] = (fx * baseline) / (units * disparity[disparity &gt; 0])
+
+*/
