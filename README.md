8c397a24fecab136d211fc5c138a3c1c3ebd4b7f
Janez Cimerman <j.cimko@gmail.com>
2019-11-05 17:16:01 +0100

    inverted y axis on poincloud,  parameters from terminal

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Running 
 
     rosrun homework1 homework1_node PATH_TO_LEFT_IMAGE PATH_TO_RIGHT_IMAGE
+
+to run with custom kernel_radius and max_disparity number the number can be added
+
+    rosrun homework1 homework1_node PATH_TO_LEFT_IMAGE PATH_TO_RIGHT_IMAGE KERNEL_RADIUS MAX_DISPARITY
--- src/homework1/src/homework1_node.cpp
+++ src/homework1/src/homework1_node.cpp
@@ -15,29 +15,37 @@ double FL = 3740.0;
 double BL = 160.0;
 
 // window size () in px to convolute with
-int KERNEL_RADIUS = 4;
+int KERNEL_RADIUS = 16;
 
 // number of disparities to consider
-int MAX_DISPARITY = 20;
+int MAX_DISPARITY = 40;
 
-std::string file = "/home/janez/Desktop/3d_win" + to_string(KERNEL_RADIUS*2) + "_max_disp" + to_string(MAX_DISPARITY) + ".xyz";
+int PX_PER_MM = 10;
 
-void logging(std::string&filename, std::string &text) {
-    try{
-        std::ofstream myfile;
-        myfile.open (filename, std::ios::app);
-        myfile << text << "\n";
-        myfile.close();
-    }
-    catch (const std::exception& e) { /* */ } 
-}
+double IMG_SCALE = 0.25;
 
+std::string file = "/home/janez/Desktop/3d_win" + to_string(KERNEL_RADIUS*2) + "_max_disp" + to_string(MAX_DISPARITY) + "_img_scale" + to_string(IMG_SCALE) + ".xyz";
 
 int main(int argc, char ** argv){
-  if (argc != 3){
+  if (argc < 3){
     cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
     return -1;
   }
+
+  if(argc == 4 || argc>5){
+    cout << "num of added parameters must be eather 1 or 3. See README" << endl;
+  }
+
+  char *p;
+
+  if(argc == 5){
+    KERNEL_RADIUS = strtol(argv[3], &p, 10);
+    MAX_DISPARITY = strtol(argv[4], &p, 10);
+  }
+
+
+  cout << "using kernel radius: " << to_string(KERNEL_RADIUS) << endl;
+  cout << "using max disperity: " << to_string(MAX_DISPARITY) << endl;
 
   Mat RS_LIMG;
   Mat RS_RIMG;
@@ -65,8 +73,8 @@ int main(int argc, char ** argv){
   }
 
   // resizing the images down for debugging
-  resize(RS_LIMG, LIMG, cv::Size(), 0.25, 0.25);
-  resize(RS_RIMG, RIMG, cv::Size(), 0.25, 0.25);
+  resize(RS_LIMG, LIMG, cv::Size(), IMG_SCALE, IMG_SCALE);
+  resize(RS_RIMG, RIMG, cv::Size(), IMG_SCALE, IMG_SCALE);
 
   // convert to Grayscale
   cvtColor(LIMG, GS_LIMG, CV_BGR2GRAY);
@@ -89,7 +97,6 @@ int main(int argc, char ** argv){
   Mat lWin(KERNEL_RADIUS*2, KERNEL_RADIUS*2, CV_8UC1, Scalar(0));
   Mat rWin(KERNEL_RADIUS*2, KERNEL_RADIUS*2, CV_8UC1, Scalar(0));
   Mat sumWin(KERNEL_RADIUS*2, KERNEL_RADIUS*2, CV_8UC1, Scalar(0));
-  // Mat depth(rows-(2*KERNEL_RADIUS), cols-(2*KERNEL_RADIUS), CV_8UC1, Scalar(0));
 
   double x = 0;
   double y = 0;
@@ -130,11 +137,12 @@ int main(int argc, char ** argv){
       int pos_disp = j+minSSDcol;
       disparities[i-KERNEL_RADIUS][j-KERNEL_RADIUS] = disp;
       
-      
+      // if disparitiy is not zero, calculate x,y,z coordinates of the current pixel
+      // offsets are included to move from OpenCV format (coordinate system in upper left corner) to poincloud format (CS in center) 
       if(disp!=0){
         x = -(BL*(double)(pos_disp-2*xOffset))/(2*(double)disp);
-        y = BL*(double)(i-yOffset)/(double)disp;
-        z = BL*FL/((double)disp*15.0);
+        y = BL*(double)(-i+yOffset)/(double)disp;
+        z = BL*FL/((double)(disp*PX_PER_MM));
       }
       else{
         x = 0;
@@ -163,11 +171,9 @@ int main(int argc, char ** argv){
     }
   }
 
-
   Mat OrigCombined;
 
   hconcat(LIMG,RIMG,OrigCombined);
-
 
   int key;
   while (1){
@@ -186,3 +192,14 @@ int main(int argc, char ** argv){
   return 0;
 }
 
+
+// not used
+void logging(std::string&filename, std::string &text) {
+    try{
+        std::ofstream myfile;
+        myfile.open (filename, std::ios::app);
+        myfile << text << "\n";
+        myfile.close();
+    }
+    catch (const std::exception& e) { /* */ } 
+}
