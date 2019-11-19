022161ba40f48d9d228163bebf0bb083e764d83a
Janez Cimerman <j.cimko@gmail.com>
2019-11-19 16:37:20 +0100

    creating filtered images while looping through variances

--- src/homework2/src/homework2_node.cpp
+++ src/homework2/src/homework2_node.cpp
@@ -9,33 +9,83 @@
 using namespace std;
 using namespace cv;
 
-int KERNEL_SIZE = 9;
+// ***************************CHANGABLES
+
+int KERNEL_SIZE = 9; //this should be an odd number
 float IMG_SCALE = 0.5;
 
+vector<double > variancesD {30.0, 40.0, 50.0, 60.0};
+vector<double > variancesR {10.0, 20.0, 30.0, 40.0};
+
+//**************************************
+
+
+
+// function that calculates distance btw point a and b
 float dis (Point a, Point b){
   int dx = a.x-b.x;
   int dy = a.y-b.y;
   return sqrt((float)(dx*dx+dy*dy));
 }
 
-
 // here im basically implementing formulas with this page and sticking to their nomenclature
-// http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/MANDUCHI1/Bilateral_Filtering.html
+// source [1]: http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/MANDUCHI1/Bilateral_Filtering.html
 // spartial gauss
-double varD = 50.0;
-double gaussC (Point a, Point b){
+double gaussC (Point a, Point b, double varD){
   double tempp = (dis(a,b)/varD);
   double ret = exp(-0.5*(tempp*tempp)); 
   return ret;
 }
 
-//photometric gauss
-double varR = 20.0;
-double gaussS (int a, int b){
+//spectral gauss see source [1]
+double gaussS (int a, int b, double varR){
   double tempp = (fabs(a-b)/varR);
   return exp(-0.5*(tempp*tempp));
 }
 
+void BilateralFilter(Mat& imgIN,
+                     Mat& imgOUT,
+                     int ker_size,
+                     double varianceR,
+                     double varianceD){
+  int cols = imgIN.cols;
+  int rows = imgIN.rows;
+
+  Mat win;
+  Mat OUT(imgIN.rows-ker_size, imgIN.cols-ker_size, CV_8UC1, Scalar(0));
+  double k = 0;
+  double tempp = 0;
+  double sum = 0;
+
+  // iterate through the image 
+  for(int x = ker_size/2; x<(cols-ker_size/2-1); x++){
+    for(int y = ker_size/2; y<(rows-ker_size/2-1); y++){
+
+      // extracting the kernel arround the current pixle
+      win = imgIN(Range(y-ker_size/2, y+ker_size/2+1), Range(x-ker_size/2, x+ker_size/2+1));
+      k = 0;
+      sum = 0;
+
+      // calculation of the filter summed up outcome for one kernel
+      for(int u = 0; u<win.cols; u++){
+        for (int v = 0; v<win.rows; v++){
+          // for explanation of these formulas see link [1]
+          tempp = gaussC(Point(u, v),Point(ker_size/2,ker_size/2), varianceD) *
+                  gaussS(win.at<uchar>(v,u), win.at<uchar>(ker_size/2,ker_size/2), varianceR);
+          k += tempp;
+          sum += tempp*win.at<uchar>(v,u);
+        }
+      }
+
+      // normalize and save the filtered pixle
+      OUT.at<uchar>(y-ker_size/2,x-ker_size/2) = (int)(sum/k);
+    }
+  }
+
+  imgOUT = OUT;
+}
+
+//**********************************************************MAIN****************************************************
 
 int main(int argc, char ** argv){
   if (argc < 2){
@@ -58,42 +108,18 @@ int main(int argc, char ** argv){
   // resizing the images down for debugging
   resize(RS_LIMG, RS_LIMG, cv::Size(), IMG_SCALE, IMG_SCALE);
 
+  // conver to grayscale
   cvtColor(RS_LIMG, GS_LIMG, CV_BGR2GRAY);
 
-  int cols = GS_LIMG.cols;
-  int rows = GS_LIMG.rows;
+  Mat filteredIMG;
 
-  Mat win;
-  Mat filteredIMG(GS_LIMG.rows-KERNEL_SIZE, GS_LIMG.cols-KERNEL_SIZE, CV_8UC1, Scalar(0));
-  double k = 0;
-  double tempp = 0;
-  double sum = 0;
-
-  // iterate through the image 
-  for(int x = KERNEL_SIZE/2; x<(cols-KERNEL_SIZE/2-1); x++){
-    for(int y = KERNEL_SIZE/2; y<(rows-KERNEL_SIZE/2-1); y++){
-
-      win = GS_LIMG(Range(y-KERNEL_SIZE/2, y+KERNEL_SIZE/2+1), Range(x-KERNEL_SIZE/2, x+KERNEL_SIZE/2+1));
-      k = 0;
-      sum = 0;
-
-      // calculation of the filter outcome
-      for(int u = 0; u<win.cols; u++){
-        for (int v = 0; v<win.rows; v++){
-          // sum+=(win.at<uchar>(v,u)-win.at<uchar>(KERNEL_SIZE/2,KERNEL_SIZE/2))*gKernel[u][v]; // debugging function
-          // sum+=dis(Point(v,u), Point(KERNEL_SIZE/2,KERNEL_SIZE/2))*gKernel[u][v]; // debugging function
-          tempp = gaussC(Point(u, v),Point(KERNEL_SIZE/2,KERNEL_SIZE/2)) * gaussS(win.at<uchar>(v,u), win.at<uchar>(KERNEL_SIZE/2,KERNEL_SIZE/2));
-          k += tempp;
-          sum += tempp*win.at<uchar>(v,u);
-        }
-      }
-
-      filteredIMG.at<uchar>(y-KERNEL_SIZE/2,x-KERNEL_SIZE/2) = (int)(sum/k);
-
+  for(double r:variancesR){
+    for(double d:variancesD){
+      BilateralFilter(GS_LIMG, filteredIMG, KERNEL_SIZE, r, d);
+      imwrite( "./././filtered_r"+to_string((int)r)+"_d"+to_string((int)d)+".jpg", filteredIMG);
     }
   }
 
-  Mat OrigCombined;
 
   int key;
   while (1){
@@ -110,16 +136,4 @@ int main(int argc, char ** argv){
   }
 
   return 0;
-}
-
-
-// not used
-void logging(std::string&filename, std::string &text) {
-    try{
-        std::ofstream myfile;
-        myfile.open (filename, std::ios::app);
-        myfile << text << "\n";
-        myfile.close();
-    }
-    catch (const std::exception& e) { /* */ } 
 }
