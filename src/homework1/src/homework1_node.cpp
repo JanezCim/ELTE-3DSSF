573971962bbaf1efb107642c6fcfee6e8e6df092
Janez Cimerman <j.cimko@gmail.com>
2020-01-02 11:08:52 +0100

    Adeded dynamic dispartiry calculation

--- src/homework1/src/homework1_node.cpp
+++ src/homework1/src/homework1_node.cpp
@@ -8,9 +8,12 @@
 using namespace std;
 using namespace cv;
 
+// if 1, calculates dynamic, if 0 calculates naive disparities
+bool CALCULATE_DYNAMIC_DISPARITY = 1;
+
 // focal lenght in px
 double FL = 3740.0;
-int PX_PER_MM = 10;
+int PX_PER_MM = 20;
 // baseline (dist btw cameras) in mm
 double BL = 160.0;
 // scale the image before computing on it (faster computation for debugging)
@@ -27,16 +30,47 @@ int MAX_DISPARITY = 40;
 int DYNAMIC_KERNEL_RADIUS = 5;
 float LAMBDA = 100;
 
+
+int DMIN = 230;
+
 // file to output the xyz file to from naive disparities
 std::string out_file = "NAIVE_DISPARITIRES_3d_win" + to_string(NAIVE_KERNEL_RADIUS*2) + "_max_disp" + to_string(MAX_DISPARITY) + "_img_scale" + to_string(IMG_SCALE) + ".xyz";
 
 // file to output the xyz file to from dynamic disparities
 std::string dynamic_file = "DYNAMIC_DISPARITIRES_3d_win" + to_string(NAIVE_KERNEL_RADIUS*2) + "_max_disp" + to_string(MAX_DISPARITY) + "_img_scale" + to_string(IMG_SCALE) + ".xyz";
 
-
+/**
+ * @brief caluclates naive dispartiy
+ * 
+ * @param l_img   left input image
+ * @param r_img   right input image
+ * @param kernel_radius   the radius of the kernel for the convolution
+ * @param max_disp  maximum allowed disparity
+ * @param disp_img  output non- normalized disparty image
+ */
 void naiveDisparity(const Mat &l_img, const Mat &r_img, const int kernel_radius, const int max_disp, Mat &disp_img);
 
+/**
+ * @brief calculates dynamic disparity
+ * 
+ * @param l_img   left input image
+ * @param r_img   right input image
+ * @param kernel_radius   the radius of the kernel for the convolution
+ * @param lambda  lambda for the dynamic calculation
+ * @param disp_img  output non- normalized disparty image
+ */
 void dynamicDisparity(const Mat &l_img, const Mat &r_img, const int kernel_radius, const float lambda, Mat &disp_img);
+
+/**
+ * @brief takes normalized disparities and converts them to 3D points which are written into the file
+ * 
+ * @param disp  normalized input disparities
+ * @param file  output file for 3d points to be saved to
+ */
+void disparitiesTo3D(const Mat &disp, string file);
+
+
+/*************************** MAIN **************************************/
 
 int main(int argc, char ** argv){
   if (argc < 3){
@@ -92,15 +126,18 @@ int main(int argc, char ** argv){
   cvtColor(RIMG, GS_RIMG, CV_BGR2GRAY);
 
   Mat disparitities;
-
-  // CHOOSE WHEATHER YOU WANT THE NAIVE OR THE DYNAMIC DISPARITY CALCULATION
-  // naiveDisparity(GS_LIMG, GS_RIMG, NAIVE_KERNEL_RADIUS, MAX_DISPARITY, disparitities);
-  dynamicDisparity(GS_LIMG, GS_RIMG, DYNAMIC_KERNEL_RADIUS, LAMBDA, disparitities);
-
   Mat norm_disparitities;
-  cv::normalize(disparitities, norm_disparitities, 255, 0, cv::NORM_MINMAX);
 
-  // disparitiesTo3D(norm_disparitities, out_file);
+  if(!CALCULATE_DYNAMIC_DISPARITY){
+    naiveDisparity(GS_LIMG, GS_RIMG, NAIVE_KERNEL_RADIUS, MAX_DISPARITY, disparitities);
+    cv::normalize(disparitities, norm_disparitities, 255, 0, cv::NORM_MINMAX);
+  }
+
+  if(CALCULATE_DYNAMIC_DISPARITY){
+    dynamicDisparity(GS_LIMG, GS_RIMG, DYNAMIC_KERNEL_RADIUS, LAMBDA, disparitities);
+    cv::normalize(disparitities, norm_disparitities, 255, 0, cv::NORM_MINMAX);
+    disparitiesTo3D(norm_disparitities, dynamic_file);
+  }
 
   Mat OrigCombined;
 
@@ -229,142 +266,157 @@ void naiveDisparity(const Mat &l_img, const Mat &r_img, const int kernel_radius,
 }
 
 
-// void disparitiesTo3D(const Mat &disp, string file){
-//   //logging
-//   std::ofstream myfile;
-//   try{
-//     myfile.open (file, std::ios::trunc);
-//   }
-//   catch (const std::exception& e) {
-//     cout << "error opening the logging file" << endl;
-//     return;
-//   } 
+void disparitiesTo3D(const Mat &disp, string file){
+  //logging
+  std::ofstream myfile;
+  try{
+    myfile.open (file, std::ios::trunc);
+  }
+  catch (const std::exception& e) {
+    cout << "error opening the logging file" << endl;
+    return;
+  } 
 
 
-//   int xOffset = disp.cols/2;
-//   int yOffset = disp.rows/2;
-//   float x,y,z;
+  int xOffset = disp.cols/2;
+  int yOffset = disp.rows/2;
+  float x,y,z;
 
-//   // float and adjusted disparity
-//   cv::Mat disp_float;
-//   disp.convertTo(disp_float, CV_32FC1);
-//   cv::Mat disp_adjusted = disp_float + dmin_;
+  cv::Mat disp_tmp;
+  disp.convertTo(disp_tmp, CV_32FC1);
+  disp_tmp = disp_tmp + DMIN;
 
-//   for(int u = 0; u<disp.cols; u++){
-//     for(int v = 0; v<disp.rows; v++){
-//       // if disparitiy is not zero, calculate x,y,z coordinates of the current pixel
-//       // offsets are included to move from OpenCV format (coordinate system in upper left corner) to poincloud format (CS in center) 
-//       float d = disp_adjusted.at<float>(v,u);
-//       if(d!=0){
-//         x = -BL * (u+xOffset * 2 - d)/(2 * d);  //-(BL*(double)(pos_disp-2*xOffset))/(2*(double)d);
-//         y = (BL*(v-yOffset))/d;  //BL*(double)(-v+yOffset)/(double)d;
-//         z = (BL*FL)/d  //BL*FL/((double)(d*PX_PER_MM));
-//       }
-//       else{
-//         x = 0;
-//         y = 0;
-//         z = 0;
-//       }
+  for(int u = 0; u<disp.cols; u++){
+    for(int v = 0; v<disp.rows; v++){
+      float d = disp_tmp.at<float>(v,u);
+      if(d!=0){
+        x = -(BL*(double)(u-xOffset))/(2*d);
+        y = BL*(double)(-v+yOffset)/d;
+        z = BL*FL/((double)(d*PX_PER_MM));
+      }
+      else{
+        x = 0;
+        y = 0;
+        z = 0;
+      }
 
-//       // logging
-//       std::string text = std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + " 0 0 0";
-//       //write into file
-//       myfile << text << "\n";  
+      // logging
+      std::string text = std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + " 0 0 0";
+      //write into file
+      myfile << text << "\n";  
     
-//     }
-//   }
-// }
-
-void dynamicDisparity(const Mat &l_img, const Mat &r_img, const int kernel_radius, const float lambda, Mat &disp_img){
-  // init
-  Size size = l_img.size();
-  int cols = l_img.cols;
-  int rows = l_img.rows;
-  Size mat_size = cv::Size(size.width - 2 * kernel_radius, size.width - 2 * kernel_radius);
-  Mat abs_diff;
-
-
-  Mat disp = cv::Mat(l_img.size(), CV_8UC1, cv::Scalar(0));
-
-  // calculate C and M for every row
-  for (int row = kernel_radius; row < rows - kernel_radius; ++row){
-    // init 2.0
-    cv::Mat cost = cv::Mat(mat_size, CV_16UC1, cv::Scalar(0));
-    cv::Mat match = cv::Mat(mat_size, CV_8UC1, cv::Scalar(0));
-
-    for (size_t cost_row_idx = 1; cost_row_idx < mat_size.height; ++cost_row_idx){
-      cost.at<ushort>(cost_row_idx, 0) = cost_row_idx * lambda;
-      match.at<uchar>(cost_row_idx, 0) = 1;
-    }
-
-    for (size_t cost_col_idx = 1; cost_col_idx < mat_size.width; ++cost_col_idx){
-      cost.at<ushort>(0, cost_col_idx) = cost_col_idx * lambda;
-      match.at<uchar>(0, cost_col_idx) = 2;
-    }
-
-    // calculating cost and match matrix for current row based on row in left and row in right image
-    for (size_t left_col_idx = 1; left_col_idx < mat_size.width; ++left_col_idx){
-      for (size_t right_col_idx = 1; right_col_idx < mat_size.height; ++right_col_idx){
-        // left and right region of interests
-        cv::Rect l_roi = cv::Rect(left_col_idx, row - kernel_radius, 2 * kernel_radius + 1, 2 * kernel_radius + 1);
-        cv::Rect r_roi = cv::Rect(right_col_idx, row - kernel_radius, 2 * kernel_radius + 1, 2 * kernel_radius + 1);
-
-        // left and right windows
-        cv::Mat leftWindow = l_img(l_roi);
-        cv::Mat rightWindow = r_img(r_roi);
-
-        // calculating sad
-        cv::absdiff(leftWindow, rightWindow, abs_diff);
-        int sad = cv::sum(abs_diff)[0];
-
-        // costs [0] - match, [1] - left occl, [2] - right occl
-        int costs[3] = { 0, 0, 0 };
-        costs[0] = cost.at<ushort>(right_col_idx - 1, left_col_idx - 1) + sad;
-        costs[1] = cost.at<ushort>(right_col_idx - 1, left_col_idx) + lambda;
-        costs[2] = cost.at<ushort>(right_col_idx, left_col_idx - 1) + lambda;
-
-        // searching for min cost based on cells calculated before
-        int min_cost = costs[0], min_idx = 0;
-        for (size_t cost_idx = 0; cost_idx < 3; cost_idx++)
-        {
-          if (costs[cost_idx] < min_cost)
-          {
-            min_cost = costs[cost_idx];
-            min_idx = cost_idx;
-          }
-        }
-
-        // assigning the cost and match to the minimal cost
-        cost.at<ushort>(right_col_idx, left_col_idx) = min_cost;
-        match.at<uchar>(right_col_idx, left_col_idx) = min_idx;
-      }
-    }
-
-    // cout << match << endl;
-
-    // path planning starting from bottom right
-    int disp_row_idx = mat_size.height - 1;
-    int disp_col_idx = mat_size.width - 1;
-
-    // iterating over columns
-    while (disp_col_idx > 0){
-      // match -> step into upper left cell, setting disp
-      if (match.at<uchar>(disp_row_idx, disp_col_idx) == 0){
-        disp.at<uchar>(row, disp_col_idx) = abs(disp_row_idx - disp_col_idx);
-        disp_row_idx--;
-        disp_col_idx--;
-      }
-      // left occl -> step into upper cell
-      else if (match.at<uchar>(disp_row_idx, disp_col_idx) == 1){
-        disp_row_idx--;
-      }
-      // right occl -> step into left cell, setting disp
-      else if (match.at<uchar>(disp_row_idx, disp_col_idx) == 2){
-        disp.at<uchar>(row, disp_col_idx) = 0;
-        disp_col_idx--;
-      }
     }
   }
-  // cout << disp << endl;
-  disp_img = disp;
+}
+
+void dynamicDisparity(const Mat &l_img, const Mat &r_img, const int kernel_radius, const float lambda, Mat &disp_img){
+  int cols = l_img.cols;
+  int rows = l_img.rows;
+  int matr_size = cols-2*kernel_radius;
+  Mat disparities = cv::Mat(l_img.size(), CV_8UC1, cv::Scalar(0));
+
+  int minSSD;
+  int currentSSD;
+  int minSSDdisparity;
+  int minSSDcol;
+
+  Mat lWin(kernel_radius*2, kernel_radius*2, CV_8UC1, Scalar(0));
+  Mat rWin(kernel_radius*2, kernel_radius*2, CV_8UC1, Scalar(0));
+  Mat sumWin(kernel_radius*2, kernel_radius*2, CV_8UC1, Scalar(0));
+
+  int min_c = 0, min_i = 0;
+
+  int disp_y, disp_x = 0;
+  int x,y,z = 0;
+
+  int yOffset = rows/2;
+  int xOffset = cols/2;
+
+  //logging
+  std::ofstream myfile;
+  try{
+    myfile.open (out_file, std::ios::trunc);
+  }
+  catch (const std::exception& e) {
+    cout << "error opening the logging file" << endl;
+  } 
+
+  // go through image, excluding the kernel width of edges
+  for (int row = kernel_radius; row < rows-kernel_radius; ++row){
+    cv::Mat c = cv::Mat(Size(matr_size,matr_size), CV_16UC1, cv::Scalar(0));
+    cv::Mat m = cv::Mat(Size(matr_size,matr_size), CV_8UC1, cv::Scalar(0));
+
+    // fill the cols costs and matches with innitial values
+    for (int i = 1; i < matr_size; ++i){
+      c.at<ushort>(0, i) = i * lambda;
+      m.at<uchar>(0, i) = 2;
+    }
+
+    // fill the rows costs and matches with innitial values
+    for (int i = 1; i < matr_size; ++i){
+      c.at<ushort>(i, 0) = i * lambda;
+      m.at<uchar>(i, 0) = 1;
+    }
+
+    // calculate m and c for each pixel for lesft and right image
+    for (int left_x = 1; left_x < matr_size; ++left_x){
+      for (int right_x = 1; right_x < matr_size; ++right_x){
+        // convolution windows
+        // lWin = l_img(Range(right_x, row-kernel_radius), Range(2*kernel_radius+1, 2*kernel_radius+1));
+        // rWin = r_img(Range(left_x, row-kernel_radius), Range(2*kernel_radius+1, 2*kernel_radius+1));
+      
+        lWin = l_img(Rect(right_x, row-kernel_radius, 2*kernel_radius+1, 2*kernel_radius+1));
+        rWin = r_img(Rect(left_x, row-kernel_radius, 2*kernel_radius+1, 2*kernel_radius+1));
+
+        absdiff(lWin, rWin, sumWin);
+        currentSSD = sum(sumWin)[0];
+
+        // determine costs
+        int costs[3] = {c.at<ushort>(right_x-1, left_x-1) + currentSSD,
+                        c.at<ushort>(right_x-1, left_x) + (int)lambda,
+                        c.at<ushort>(right_x, left_x-1) + (int)lambda};
+        
+        // cout << costs << endl;
+
+
+        int min_c = costs[0];
+        int min_i = 0;
+        for (int i = 0; i<3; i++){
+          if (costs[i] < min_c){
+            min_c = costs[i];
+            min_i = i;
+          }
+        }
+        m.at<uchar>(right_x, left_x) = min_i;
+        c.at<ushort>(right_x, left_x) = min_c;
+      }
+    }
+
+    // cout << m << endl;
+
+    // starting from the bottom right
+    disp_y = matr_size-1;
+    disp_x = matr_size-1;
+
+    int cur_disp = 0;
+    // if the x dispartity is not zero, continue checking
+    while (disp_x > 0){
+      // match
+      if (m.at<uchar>(disp_y, disp_x) == 0){
+        cur_disp = abs(disp_y - disp_x);
+        disp_x--;
+        disp_y--;
+      }
+      else if (m.at<uchar>(disp_y, disp_x) == 2){
+        cur_disp = 0;
+        disp_x--;
+      }
+      else if (m.at<uchar>(disp_y, disp_x) == 1){
+        disp_y--;
+      }
+      disparities.at<uchar>(row, disp_x) = cur_disp;
+    }
+  }
+  // cout << disparities << endl;
+  disp_img = disparities;
 }
